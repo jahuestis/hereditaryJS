9dc97cc7f10b7be65d886b5a98806dc71984580b
jahuestis <160439894+jahuestis@users.noreply.github.com>
2025-02-01 20:50:50 -0500

    Improved window styles

--- hereditary/hereditary.js
+++ hereditary/hereditary.js
@@ -3,8 +3,8 @@ import Window from './window.js';
 
 const windowWidth = 300;
 const windowHeight = 300;
-let window1 = new Window('window1', Math.random() * window.innerWidth, Math.random() * window.innerHeight, windowWidth, windowHeight);
-let window2 = new Window('window2', Math.random() * window.innerWidth, Math.random() * window.innerHeight, windowWidth, windowHeight);
+let window1 = new Window('daisy 1', Math.random() * window.innerWidth, Math.random() * window.innerHeight, windowWidth, windowHeight);
+let window2 = new Window('daisy 2', Math.random() * window.innerWidth, Math.random() * window.innerHeight, windowWidth, windowHeight);
 
 let window1BodyText = document.createElement('p');
 window1BodyText.textContent = 'hello world';
